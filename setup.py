0b445dc57786778687900b47706b9ecb5b9d29ce
Barry Warsaw <barry@python.org>
2016-04-03 15:13:51 -0400

    Floor the Falcon dependency.

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ case second `m'.  Any other spelling is incorrect.""",
         },
     install_requires = [
         'alembic',
-        'falcon',
+        'falcon>=1.0.0rc1',
         'flufl.bounce',
         'flufl.i18n',
         'flufl.lock',
