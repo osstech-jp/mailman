ffc8ed1347121079c047ec454f94e27bf54a9d42
Barry Warsaw <barry@python.org>
2016-11-25 09:05:26 -0500

    Handle a Python 3.6 difference in interact().
    Also:
    
    * Fix a regexp that 3.6 raises an exception on.
    * Remove a deprecated attribute from passlib.cfg.

--- src/mailman/utilities/interact.py
+++ src/mailman/utilities/interact.py
@@ -22,6 +22,7 @@ import sys
 import code
 
 from contextlib import suppress
+from inspect import signature
 from mailman import public
 
 
@@ -67,4 +68,10 @@ def interact(upframe=True, banner=DEFAULT_BANNER, overrides=None):
 Python %s on %s
 Type "help", "copyright", "credits" or "license" for more information.''' % (
             sys.version, sys.platform)
-    interp.interact(banner)
+    # Python 3.6 added an exitmsg keyword but we don't currently support
+    # configuring it.  For consistency between Python 3.6 and earlier
+    # versions, suppress the exit message if possible.
+    kws = dict(banner=banner)
+    if 'exitmsg' in signature(interp.interact).parameters:
+        kws['exitmsg'] = ''
+    interp.interact(**kws)
