43c9a6ab8d410e7a65912be8431c5d32f9c96399
Abhilash Raj <raj.abhilash1@gmail.com>
2019-09-06 20:32:27 -0700

    Version bump post release.

--- src/mailman/version.py
+++ src/mailman/version.py
@@ -18,7 +18,7 @@
 """Mailman version strings."""
 
 # Mailman version.
-VERSION = '3.3.0'
+VERSION = '3.3.1'
 CODENAME = 'La Villa Strangiato'
 
 # And as a hex number in the manner of PY_VERSION_HEX.
@@ -31,7 +31,7 @@ FINAL = 0xf
 
 MAJOR_REV = 3
 MINOR_REV = 3
-MICRO_REV = 0
+MICRO_REV = 1
 REL_LEVEL = RC
 # At most 15 beta releases!
 REL_SERIAL = FINAL
